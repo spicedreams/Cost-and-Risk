a8f4642ec56765f090e5e5fdbcdf92be9ff03a3f
spicedreams <graham@harris.net.nz>
2026-08-13 14:49:29 +1200

    2026-08-13 1449 project file browser, many small UI tweaks, Test generator rewritten Risk, Issue sections to be more plausible .gitignore updated to ignore R, safe, generated & synthetic directories and data files: .sqlite, .csv, .xlsx

--- cost-risk-mc.bat
+++ cost-risk-mc.bat
@@ -1,10 +1,13 @@
 @echo off
+c:
+cd \Users\ghar115\professional\risk\cost-risk
+subst R: C:\Users\ghar115\professional\risk\cost-risk\R\portable-r-4.6.1-win-x64
 :: Find the drive letter this script is currently running from
 set "SCRIPT_DIR=%~dp0"
 
 :: Launch the portable R executable and pass your main script
-"%SCRIPT_DIR%\bin\x64\Rscript.exe" "%SCRIPT_DIR%\App\cost-risk-mc.R"
-
+:: "%SCRIPT_DIR%\bin\x64\Rscript.exe" "%SCRIPT_DIR%\App\cost-risk-mc.R"
+R\portable-r-4.6.1-win-x64\bin\RScript.exe cost-risk-mc.R
 pause
 
 :: C:\Users\ghar115\professional\risk\cost-risk>R\portable-r-4.6.1-win-x64\bin\RScript.exe cost-risk-mc.R
